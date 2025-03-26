9bacde296c666466e18a816a000b1ba2ba1cfa5d
Grzegorz_Szwed <greg152295@gmail.com>
2025-03-26 12:27:04 +0100

    refactor of code

--- script/product.js
+++ script/product.js
@@ -1,11 +1,24 @@
-const urlParams = new URLSearchParams(window.location.search);
-const productId = urlParams.get("id");
-const productName = urlParams.get("name");
-const productPrice = urlParams.get("price");
-const productImg = urlParams.get("img");
+const utilityMethods = {
+    initializeUtilities() {
 
-document.getElementById("product-name").textContent = productName;
-document.getElementById("product-id").textContent = "ID: " + productId;
-document.getElementById("product-price").textContent = "Cena: " + productPrice;
-document.getElementById("product-img").src = productImg;
-document.getElementById("product-img").alt = productName;
+        const urlParams = new URLSearchParams(window.location.search);
+        const productId = urlParams.get("id");
+        const productName = urlParams.get("name");
+        const productPrice = urlParams.get("price");
+        const productImg = urlParams.get("img");
+        const productImgHover = urlParams.get('imgHover');
+
+        document.getElementById("product-name").textContent = productName;
+        document.getElementById("product-id").textContent = "ID: " + productId;
+        document.getElementById("product-price").textContent = "Cena: " + productPrice;
+        document.getElementById("product-img").src = productImg;
+        document.getElementById("product-img").alt = productImg;
+        document.getElementById("product-img-hover").alt = productImgHover;
+        document.getElementById("product-img-hover").src = productImgHover;
+    }
+}
+
+
+const main = () => utilityMethods.initializeUtilities();
+
+document.addEventListener("DOMContentLoaded", () => main());
--- script/products.js
+++ script/products.js
@@ -1,55 +1,79 @@
+const domElements = { productList: document.getElementById("product-list") };
+
 const products = [
-    { id: 1111, name: "Gitara Elektryczna", alt: "elektryczna gitara", price: "1200 PLN", img: "https://images.unsplash.com/photo-1568193755668-aae18714a9f1" },
-    { id: 1112, name: "Gitara Basowa", alt: "basowa gitara", price: "2500 PLN", img: "https://plus.unsplash.com/premium_photo-1664194584456-c25febbccb2a" },
-    { id: 1113, name: "Gitara Elektryczna", alt: "elektryczna gitara", price: "900 PLN", img: "https://images.unsplash.com/photo-1508186736123-44a5fcb36f9f" },
-    { id: 1114, name: "Gitara Elektryczna", alt: "elektryczna gitara", price: "1800 PLN", img: "https://plus.unsplash.com/premium_photo-1681335985809-22600ebed275" },
-    { id: 1115, name: "Gitara Elektryczna", alt: "elektryczna gitara", price: "1800 PLN", img: "https://images.unsplash.com/photo-1619361369057-a86afad4be8a" },
-    { id: 1116, name: "Gitara Elektryczna", alt: "elektryczna gitara", price: "1800 PLN", img: "https://images.unsplash.com/photo-1550985616-10810253b84d" },
-    { id: 1117, name: "Gitara Elektryczna", alt: "elektryczna gitara", price: "1800 PLN", img: "https://images.unsplash.com/photo-1601956349578-c31521587860" },
-    { id: 1118, name: "Gitara Elektryczna", alt: "elektryczna gitara", price: "1800 PLN", img: "https://images.unsplash.com/photo-1616533719060-a7fc492e9299" },
-    { id: 1119, name: "Gitara Elektryczna", alt: "elektryczna gitara", price: "1800 PLN", img: "https://images.unsplash.com/photo-1582379119910-f752fdfc8c5f" },
-    { id: 1120, name: "Gitara Elektryczna", alt: "elektryczna gitara", price: "1800 PLN", img: "https://images.unsplash.com/photo-1583679670276-90aa14338851" },
-    { id: 1121, name: "Gitara Elektryczna", alt: "elektryczna gitara", price: "1800 PLN", img: "https://plus.unsplash.com/premium_photo-1681335986095-5a9585e77246" },
-    { id: 1123, name: "Gitara Elektryczna", alt: "elektryczna gitara", price: "1800 PLN", img: "https://images.unsplash.com/photo-1583067525935-d926610ff2f1" },
-    { id: 1124, name: "Gitara Elektryczna", alt: "elektryczna gitara", price: "1800 PLN", img: "https://images.unsplash.com/photo-1618530089935-3030738b8c7b" },
-    { id: 1125, name: "Gitara Elektryczna", alt: "elektryczna gitara", price: "1800 PLN", img: "https://images.unsplash.com/photo-1520166012956-add9ba0835cb" },
-    { id: 1126, name: "Gitara Elektryczna", alt: "elektryczna gitara", price: "1800 PLN", img: "https://images.unsplash.com/photo-1459230995443-0f098868784d" },
-    { id: 1127, name: "Gitara Elektryczna", alt: "elektryczna gitara", price: "1800 PLN", img: "https://images.unsplash.com/photo-1540281811908-3614b2e5c56b" },
-    { id: 1128, name: "Gitara Elektryczna", alt: "elektryczna gitara", price: "1800 PLN", img: "https://images.unsplash.com/photo-1613032970340-7846189c1cbe" },
-    { id: 1129, name: "Gitara Elektryczna", alt: "elektryczna gitara", price: "1800 PLN", img: "https://images.unsplash.com/photo-1616066959736-5983a58b2071" },
-    { id: 1130, name: "Gitara Elektryczna", alt: "elektryczna gitara", price: "1800 PLN", img: "https://images.unsplash.com/flagged/photo-1552331619-7f97d9a4c9c6" },
-    { id: 1131, name: "Gitara Elektryczna", alt: "elektryczna gitara", price: "1800 PLN", img: "https://images.unsplash.com/photo-1606041212382-db0c175d8712" },
-    { id: 1132, name: "Gitara Elektryczna", alt: "elektryczna gitara", price: "1800 PLN", img: "https://images.unsplash.com/photo-1618530627969-d1de1bf1b3ca" }
+    { id: 1111, name: "Gitara Elektryczna One", alt: "Gitara Elektryczna One", price: "1200 PLN", img: "https://images.unsplash.com/photo-1568193755668-aae18714a9f1?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1", imgHover: "https://images.unsplash.com/photo-1574624136404-74cc2f967ba5" },
+    { id: 1112, name: "Gitara Basowa", alt: "basowa gitara", price: "2500 PLN", img: "https://plus.unsplash.com/premium_photo-1664194584456-c25febbccb2a", imgHover: "https://images.unsplash.com/photo-1574624136404-74cc2f967ba5" },
+    { id: 1113, name: "Gitara Elektryczna Two", alt: "Gitara Elektryczna Two", price: "900 PLN", img: "https://images.unsplash.com/photo-1508186736123-44a5fcb36f9f", imgHover: "https://images.unsplash.com/photo-1574624136404-74cc2f967ba5" },
+    { id: 1114, name: "Gitara Elektryczna Three", alt: "Gitara Elektryczna Three", price: "1801 PLN", img: "https://plus.unsplash.com/premium_photo-1681335985809-22600ebed275", imgHover: "https://images.unsplash.com/photo-1574624136404-74cc2f967ba5" },
+    { id: 1115, name: "Gitara Elektryczna Four", alt: "Gitara Elektryczna Four", price: "1802 PLN", img: "https://images.unsplash.com/photo-1619361369057-a86afad4be8a?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1", imgHover: "https://images.unsplash.com/photo-1574624136404-74cc2f967ba5?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" },
+    { id: 1116, name: "Gitara Elektryczna Five", alt: "Gitara Elektryczna Five", price: "1803 PLN", img: "https://images.unsplash.com/photo-1550985616-10810253b84d?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1", imgHover: "https://images.unsplash.com/photo-1574624136404-74cc2f967ba5?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" },
+    { id: 1117, name: "Gitara Elektryczna Six", alt: "Gitara Elektryczna Six", price: "1804 PLN", img: "https://images.unsplash.com/photo-1601956349578-c31521587860", imgHover: "https://images.unsplash.com/photo-1574624136404-74cc2f967ba5" },
+    { id: 1118, name: "Gitara Elektryczna Seven", alt: "Gitara Elektryczna Seven", price: "1805 PLN", img: "https://images.unsplash.com/photo-1616533719060-a7fc492e9299", imgHover: "https://images.unsplash.com/photo-1574624136404-74cc2f967ba5" },
+    { id: 1119, name: "Gitara Elektryczna Eight", alt: "Gitara Elektryczna Eight", price: "1806 PLN", img: "https://images.unsplash.com/photo-1582379119910-f752fdfc8c5f", imgHover: "https://images.unsplash.com/photo-1574624136404-74cc2f967ba5" },
+    { id: 1120, name: "Gitara Elektryczna Nine", alt: "Gitara Elektryczna Nine", price: "1807 PLN", img: "https://images.unsplash.com/photo-1583679670276-90aa14338851", imgHover: "https://images.unsplash.com/photo-1574624136404-74cc2f967ba5" },
+    { id: 1121, name: "Gitara Elektryczna Ten", alt: "Gitara Elektryczna Ten", price: "1808 PLN", img: "https://plus.unsplash.com/premium_photo-1681335986095-5a9585e77246", imgHover: "https://images.unsplash.com/photo-1574624136404-74cc2f967ba5" },
+    { id: 1122, name: "Gitara Elektryczna Eleven", alt: "Gitara Elektryczna Eleven", price: "1809 PLN", img: "https://images.unsplash.com/photo-1583067525935-d926610ff2f1", imgHover: "https://images.unsplash.com/photo-1574624136404-74cc2f967ba5" },
+    { id: 1123, name: "Gitara Elektryczna Thirteen", alt: "Gitara Elektryczna Thirteen", price: "1811 PLN", img: "https://images.unsplash.com/photo-1618530089935-3030738b8c7b", imgHover: "https://images.unsplash.com/photo-1574624136404-74cc2f967ba5" },
+    { id: 1124, name: "Gitara Elektryczna Fourteen", alt: "Gitara Elektryczna Fourteen", price: "1812 PLN", img: "https://images.unsplash.com/photo-1520166012956-add9ba0835cb", imgHover: "https://images.unsplash.com/photo-1574624136404-74cc2f967ba5" },
+    { id: 1125, name: "Gitara Elektryczna Fifteen", alt: "Gitara Elektryczna Fifteen", price: "1813 PLN", img: "https://images.unsplash.com/photo-1459230995443-0f098868784d", imgHover: "https://images.unsplash.com/photo-1574624136404-74cc2f967ba5" },
+    { id: 1126, name: "Gitara Elektryczna Sixteen", alt: "Gitara Elektryczna Sixteen", price: "1814 PLN", img: "https://images.unsplash.com/photo-1540281811908-3614b2e5c56b", imgHover: "https://images.unsplash.com/photo-1574624136404-74cc2f967ba5" },
+    { id: 1127, name: "Gitara Elektryczna Seventeen", alt: "Gitara Elektryczna Seventenn", price: "1815 PLN", img: "https://images.unsplash.com/photo-1613032970340-7846189c1cbe", imgHover: "https://images.unsplash.com/photo-1574624136404-74cc2f967ba5" },
+    { id: 1128, name: "Gitara Elektryczna Eighteen", alt: "Gitara Elektryczna Eighteen", price: "1816 PLN", img: "https://images.unsplash.com/photo-1616066959736-5983a58b2071", imgHover: "https://images.unsplash.com/photo-1574624136404-74cc2f967ba5" },
+    { id: 1129, name: "Gitara Elektryczna Nineteen", alt: "Gitara Elektryczna Nineteen", price: "1817 PLN", img: "https://images.unsplash.com/flagged/photo-1552331619-7f97d9a4c9c6", imgHover: "https://images.unsplash.com/photo-1574624136404-74cc2f967ba5" },
+    { id: 1130, name: "Gitara Elektryczna Twenty", alt: "Gitara Elektryczna Twenty", price: "1818 PLN", img: "https://images.unsplash.com/photo-1606041212382-db0c175d8712", imgHover: "https://images.unsplash.com/photo-1574624136404-74cc2f967ba5" },
+    { id: 1131, name: "Gitara Elektryczna Twenty-one", alt: "Gitara Elektryczna Twenty-one", price: "1819 PLN", img: "https://images.unsplash.com/photo-1618530627969-d1de1bf1b3ca", imgHover: "https://images.unsplash.com/photo-1574624136404-74cc2f967ba5" }
 ];
 
-const productList = document.getElementById("product-list");
+const productRenderer = {
+    renderProducts() {
+        const { productList } = domElements;
 
-products.forEach((product) => {
-    const productElement = document.createElement("div");
-    productElement.classList.add("product");
+        if (!productList) return;
 
-    productElement.innerHTML = `
-        <a href="product.html?id=${product.id}&name=${encodeURIComponent(product.name)}&alt=${encodeURIComponent(product.alt)}&price=${encodeURIComponent(product.price)}&img=${encodeURIComponent(product.img)}">
-            <img class="wrapper__products-img" src="${product.img}" alt="${product.name}" loading="lazy">
-        </a>
-        <h3 class="wrapper__products-title">${product.name}</h3>
-        <p class="wrapper__products-price">Cena: ${product.price}</p>
-        <p class="wrapper__products-id">ID: ${product.id}</p>
-    `;
+        const fragment = document.createDocumentFragment();
 
-    productList.appendChild(productElement);
-});
+        products.forEach((product) => {
+            const productElement = document.createElement("div");
+            productElement.classList.add("product");
 
-const observer = new IntersectionObserver(
-    (entries) => {
-        entries.forEach((entry) => {
-            if (entry.isIntersecting) {
-                entry.target.classList.add("visible");
-                observer.unobserve(entry.target);
-            }
+            productElement.innerHTML = `
+                <a href="product.html?id=${product.id}&name=${encodeURIComponent(product.name)}&alt=${encodeURIComponent(product.alt)}&price=${encodeURIComponent(product.price)}&img=${encodeURIComponent(product.img)}&imgHover=${encodeURIComponent(product.imgHover)}">
+                    <img class="wrapper__products-img" src="${product.img}" alt="${product.name}" loading="lazy">
+                </a>
+                <h3 class="wrapper__products-title">${product.name}</h3>
+                <p class="wrapper__products-price">Cena: ${product.price}</p>
+                <p class="wrapper__products-id">ID: ${product.id}</p>
+            `;
+
+            fragment.appendChild(productElement);
         });
-    },
-    { threshold: 0.2, rootMargin: "0px 0px 100px 0px" }
-);
 
-document.querySelectorAll(".product").forEach((product) => observer.observe(product));
+        productList.appendChild(fragment);
+    },
+};
+
+const productObservers = {
+    initializeObservers() {
+        const observer = new IntersectionObserver(
+            (entries) => {
+                entries.forEach((entry) => {
+                    if (entry.isIntersecting) {
+                        entry.target.classList.add("visible");
+
+                        observer.unobserve(entry.target);
+                        console.log('dupa');
+                    }
+                });
+            },
+            { threshold: 0.2 }
+        );
+        document.querySelectorAll(".product").forEach((product) => observer.observe(product));
+    }
+};
+
+const main = () => {
+    productRenderer.renderProducts();
+    productObservers.initializeObservers();
+};
+
+document.addEventListener("DOMContentLoaded", main);
